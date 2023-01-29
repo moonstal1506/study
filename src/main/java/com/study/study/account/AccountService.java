43bb493ca53197a4f86b189c8fdf66aeb993c6c7
moonstal1506 <moonstal1506@gmail.com>
2023-01-30 00:07:13 +0900

    회원 가입 인증 메일 확인 화면

--- src/main/java/com/study/study/account/AccountService.java
+++ src/main/java/com/study/study/account/AccountService.java
@@ -6,6 +6,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,6 +16,7 @@ public class AccountService {
     private final JavaMailSender javaMailSender;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public void processNewAccount(SignUpForm signUpForm) {
         Account newAccount = saveNewAccount(signUpForm);
         newAccount.generateEmailCheckToken();
@@ -39,7 +41,7 @@ public class AccountService {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(newAccount.getEmail());
         mailMessage.setSubject("합격스터디, 회원 가입 인증");
-        mailMessage.setText("check-email-token?="+ newAccount.getEmailCheckToken()+
+        mailMessage.setText("/check-email-token?token="+ newAccount.getEmailCheckToken()+
                 "&email=" + newAccount.getEmail());
         javaMailSender.send(mailMessage);
     }
--- src/test/java/com/study/study/account/AccountControllerTest.java
+++ src/test/java/com/study/study/account/AccountControllerTest.java
@@ -68,6 +68,7 @@ class AccountControllerTest {
         assertNotNull(account);
         assertNotEquals(account.getPassword(),"12345678");
         assertTrue(accountRepository.existsByEmail("email@email.com"));
+        assertNotNull(account.getEmailCheckToken());
         then(javaMailSender).should().send(any(SimpleMailMessage.class));
     }
 }
