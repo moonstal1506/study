88132b86be27e595623b04f3530f0aaacf8b657e
moonstal1506 <moonstal1506@gmail.com>
2023-02-07 22:51:19 +0900

    가입 확인 이메일 재전송

--- src/main/java/com/study/study/account/AccountService.java
+++ src/main/java/com/study/study/account/AccountService.java
@@ -44,7 +44,7 @@ public class AccountService {
         return newAccount;
     }
 
-    private void sendSignUpConfirmEmail(Account newAccount) {
+    public void sendSignUpConfirmEmail(Account newAccount) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(newAccount.getEmail());
         mailMessage.setSubject("합격스터디, 회원 가입 인증");
--- src/main/java/com/study/study/domain/Account.java
+++ src/main/java/com/study/study/domain/Account.java
@@ -3,6 +3,7 @@ package com.study.study.domain;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -56,8 +57,11 @@ public class Account {
 
     private boolean studyUpdatedByWeb;
 
+    private LocalDateTime emailCheckTokenGeneratedAt;
+
     public void generateEmailCheckToken() {
         this.emailCheckToken = UUID.randomUUID().toString();
+        this.emailCheckTokenGeneratedAt = LocalDateTime.now();
     }
 
     public void completeSignUp() {
@@ -67,5 +71,9 @@ public class Account {
 
     public boolean isValidToken(String token) {
         return this.emailCheckToken.equals(token);
+    }
+
+    public boolean canSendConfirmEmail() {
+        return this.emailCheckTokenGeneratedAt.isBefore(LocalDateTime.now().minusHours(1));
     }
 }
