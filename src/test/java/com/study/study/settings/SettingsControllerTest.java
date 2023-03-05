ff400f1305a8fa4cb9ead67110073faff34329b2
moonstal1506 <moonstal1506@gmail.com>
2023-03-05 09:56:53 +0900

    패스워드 수정 테스트

--- src/test/java/com/study/study/settings/SettingsControllerTest.java
+++ src/test/java/com/study/study/settings/SettingsControllerTest.java
@@ -2,7 +2,6 @@ package com.study.study.settings;
 
 import com.study.study.WithAccount;
 import com.study.study.account.AccountRepository;
-import com.study.study.account.AccountService;
 import com.study.study.domain.Account;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,10 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -28,6 +27,9 @@ class SettingsControllerTest {
 
     @Autowired
     AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @AfterEach
     void afterEach() {
@@ -48,7 +50,7 @@ class SettingsControllerTest {
     @DisplayName("프로필 수정하기 - 입력값 정상")
     @Test
     void updateProfile() throws Exception {
-       String bio = "짧은 소개를 수정하는 경우";
+        String bio = "짧은 소개를 수정하는 경우";
 
         mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
                         .param("bio", bio)
@@ -60,6 +62,7 @@ class SettingsControllerTest {
         Account msj = accountRepository.findByNickname("msj");
         assertEquals(bio, msj.getBio());
     }
+
     @WithAccount("msj")
     @DisplayName("프로필 수정하기 - 입력값 에러")
     @Test
@@ -76,5 +79,46 @@ class SettingsControllerTest {
 
         Account msj = accountRepository.findByNickname("msj");
         assertNull(msj.getBio());
+    }
+
+    @WithAccount("msj")
+    @DisplayName("패스워드 수정 폼")
+    @Test
+    void updatePassword() throws Exception {
+        mockMvc.perform(get(SettingsController.SETTINGS_PASSWORD_URL))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("passwordForm"));
+    }
+
+    @WithAccount("msj")
+    @DisplayName("패스워드 수정하기 - 입력값 정상")
+    @Test
+    void updatePassword_success() throws Exception {
+        mockMvc.perform(post(SettingsController.SETTINGS_PASSWORD_URL)
+                        .param("newPassword", "12345678")
+                        .param("newPasswordConfirm", "12345678")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(SettingsController.SETTINGS_PASSWORD_URL))
+                .andExpect(flash().attributeExists("message"));
+
+        Account msj = accountRepository.findByNickname("msj");
+        assertTrue(passwordEncoder.matches("12345678", msj.getPassword()));
+    }
+
+    @WithAccount("msj")
+    @DisplayName("패스워드 수정하기 - 입력값 에러 - 패스워드 불일치")
+    @Test
+    void updatePassword_fail() throws Exception {
+        mockMvc.perform(post(SettingsController.SETTINGS_PASSWORD_URL)
+                        .param("newPassword", "12345678")
+                        .param("newPasswordConfirm", "11111111")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name(SettingsController.SETTINGS_PASSWORD_VIEW_NAME))
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeExists("passwordForm"))
+                .andExpect(model().attributeExists("account"));
     }
 }
