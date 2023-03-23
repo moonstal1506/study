b7ba2eec09685060216f797da05f1c677283c08d
moonstal1506 <moonstal1506@gmail.com>
2023-03-23 20:13:09 +0900

    지역 도메인

---
+++ src/main/java/com/study/study/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.study.study.zone;
+
+import com.studyolle.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
