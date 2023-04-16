865661b85364172bd2192620df9188d8f37765df
moonstal1506 <moonstal1506@gmail.com>
2023-04-16 21:06:06 +0900

    모임 만들기 폼 서브밋

---
+++ src/main/java/com/study/study/event/EventRepository.java
@@ -0,0 +1,9 @@
+package com.study.study.event;
+
+import com.study.study.domain.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
---
+++ src/main/java/com/study/study/event/EventService.java
@@ -0,0 +1,25 @@
+package com.study.study.event;
+
+import com.study.study.domain.Account;
+import com.study.study.domain.Event;
+import com.study.study.domain.Study;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+    public Event createEvent(Event event, Study study, Account account) {
+        event.setCreateBy(account);
+        event.setCreatedDateTime(LocalDateTime.now());
+        event.setStudy(study);
+        return eventRepository.save(event);
+    }
+}
