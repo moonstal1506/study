df4733c5cd6b765d6152bd466246f6b697560e79
moonstal1506 <moonstal1506@gmail.com>
2023-04-29 23:37:35 +0900

    알림 아이콘 변경

--- src/main/java/com/study/study/modules/notification/NotificationRepository.java
+++ src/main/java/com/study/study/modules/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.study.study.modules.notification;
 
+import com.study.study.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    long countByAccountAndChecked(Account account, boolean checked);
 }
