e6b87e0595808f498c2d05b70f867c2c3b07e934
moonstal1506 <moonstal1506@gmail.com>
2023-05-04 22:48:40 +0900

    검색 기능 구현

--- src/main/java/com/study/study/infra/config/SecurityConfig.java
+++ src/main/java/com/study/study/infra/config/SecurityConfig.java
@@ -27,8 +27,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .mvcMatchers("/", "/login", "/sign-up", "/check-email-token",
-                        "email-login", "check-email-login", "/login-link").permitAll()
-                .mvcMatchers(HttpMethod.GET, "profile/*").permitAll()
+                        "email-login", "check-email-login", "/search/study").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                 .anyRequest().authenticated();
 
         http.formLogin()
---
+++ src/main/java/com/study/study/modules/study/StudyRepositoryExtension.java
@@ -0,0 +1,12 @@
+package com.study.study.modules.study;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+public interface StudyRepositoryExtension {
+
+    List<Study> findByKeyword(String keyword);
+
+}
