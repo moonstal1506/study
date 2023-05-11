d2eeebe58209075ccf2af74dc226f13a6c74b268
moonstal1506 <moonstal1506@gmail.com>
2023-05-11 23:30:42 +0900

    로그인 후 첫 페이지

--- src/main/java/com/study/study/modules/event/EnrollmentRepository.java
+++ src/main/java/com/study/study/modules/event/EnrollmentRepository.java
@@ -2,10 +2,16 @@ package com.study.study.modules.event;
 
 
 import com.study.study.modules.account.Account;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     boolean existsByEventAndAccount(Event event, Account account);
 
     Enrollment findByEventAndAccount(Event event, Account account);
+
+    @EntityGraph("Enrollment.withEventAndStudy")
+    List<Enrollment> findByAccountAndAcceptedOrderByEnrolledAtDesc(Account account, boolean accepted);
 }
--- src/main/java/com/study/study/modules/study/StudyRepositoryExtensionImpl.java
+++ src/main/java/com/study/study/modules/study/StudyRepositoryExtensionImpl.java
@@ -4,13 +4,16 @@ import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.JPQLQuery;
 import com.study.study.modules.account.QAccount;
 import com.study.study.modules.tag.QTag;
+import com.study.study.modules.tag.Tag;
 import com.study.study.modules.zone.QZone;
+import com.study.study.modules.zone.Zone;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
+import java.util.Set;
 
 public class  StudyRepositoryExtensionImpl extends QuerydslRepositorySupport implements StudyRepositoryExtension {
 
@@ -32,5 +35,20 @@ public class  StudyRepositoryExtensionImpl extends QuerydslRepositorySupport imp
         JPQLQuery<Study> pageableQuery = getQuerydsl().applyPagination(pageable, query);
         QueryResults<Study> fetchResults = pageableQuery.fetchResults();
         return new PageImpl<>(fetchResults.getResults(), pageable, fetchResults.getTotal());
+    }
+
+    @Override
+    public List<Study> findByAccount(Set<Tag> tags, Set<Zone> zones) {
+        QStudy study = QStudy.study;
+        JPQLQuery<Study> query = from(study).where(study.published.isTrue()
+                        .and(study.closed.isFalse())
+                        .and(study.tags.any().in(tags))
+                        .and(study.zones.any().in(zones)))
+                .leftJoin(study.tags, QTag.tag).fetchJoin()
+                .leftJoin(study.zones, QZone.zone).fetchJoin()
+                .orderBy(study.publishedDateTime.desc())
+                .distinct()
+                .limit(9);
+        return query.fetch();
     }
 }
